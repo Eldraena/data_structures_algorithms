146368838a4f89e714b89c83313342cd74e4fe96
netwanderer <128877043+nguyennamtien91@users.noreply.github.com>
2023-05-28 08:09:03 +0700

    Update floyd_warshall.cpp

--- graph_theory/floyd_warshall.cpp
+++ graph_theory/floyd_warshall.cpp
@@ -1,11 +1,21 @@
-  for (k = 0; k < nV; k++) 
-  {
-    for (i = 0; i < nV; i++) 
+    void floyd_warshall(int n)
     {
-      for (j = 0; j < nV; j++) 
-      {
-        if (matrix[i][k] + matrix[k][j] < matrix[i][j])
-          matrix[i][j] = matrix[i][k] + matrix[k][j];
-      }
+        for(int k = 0; k < n; k++)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if(i == j)
+                    {
+                        min_distance[i][j] = 0;
+                        continue;
+                    }
+                    if(min_distance[i][k] == INT_MAX || min_distance[k][j] == INT_MAX)
+                        continue;
+                    if(min_distance[i][j] > min_distance[i][k] + min_distance[k][j])
+                        min_distance[i][j] = min_distance[i][k] + min_distance[k][j];
+                }
+            }
+        }
     }
-  }
