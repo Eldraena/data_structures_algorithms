b6bac197b81e5dbc332d0fccc1f4946c66a411d3
netwanderer <128877043+nguyennamtien91@users.noreply.github.com>
2023-06-24 23:23:26 +0700

    Update permutation.cpp

--- combinatorics/permutation.cpp
+++ combinatorics/permutation.cpp
@@ -40,9 +40,9 @@ cin >> n;
 
 for (int i = 0; i < (1<<n); i++) 
 {
-  for(int j = 0; j < n; j++) 
+  	for(int j = 0; j < n; j++) 
 	{
-	  cout << ((i >> j) & 1) << " "; 
+		cout << ((i >> j) & 1) << " "; 
 	}
 	cout << endl;
 }
