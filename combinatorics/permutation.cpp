51f9ff7443e0f44009abd829577ace9246dce8f9
netwanderer <128877043+nguyennamtien91@users.noreply.github.com>
2023-07-01 18:14:24 +0700

    Update permutation.cpp

--- combinatorics/permutation.cpp
+++ combinatorics/permutation.cpp
@@ -33,16 +33,3 @@ public:
         return res;     
     }
 };
-
-//Special case: permutation binary
-int n;
-cin >> n;
-
-for (int i = 0; i < (1<<n); i++) 
-{
-   for(int j = 0; j < n; j++) 
-   {
-      cout << ((i >> j) & 1) << " "; 
-   }
-   cout << endl;
-}
