f283275a28777ce04d7acf198971481523838892
netwanderer <128877043+nguyennamtien91@users.noreply.github.com>
2023-08-21 21:27:16 +0700

    Update vector.cpp

--- data_structures/vector.cpp
+++ data_structures/vector.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+sort(block_and_sheet.begin(), block_and_sheet.end(), [](const pair<int, pair<int, int>>& a, const pair<int, pair<int, int>>& b)
+{
+    if(a.second.second != b.second.second)
+        return a.second.second < b. second.second;
+    else
+        return a.first < b.first;
+});
  
 int main()
 {
