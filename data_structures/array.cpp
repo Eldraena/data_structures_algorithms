4e6a875a2a266c3d3834131904a4464b14d1bb38
netwanderer <128877043+nguyennamtien91@users.noreply.github.com>
2023-08-27 16:16:59 +0700

    Update array.cpp

--- data_structures/array.cpp
+++ data_structures/array.cpp
@@ -1,1 +1,13 @@
-int A[10] = { [0] = 0, [1 ... 9] = INT_MAX };
+#include <stdio.h>
+void main(void)
+{
+    int numbers[100] = {1, 2, 3, [3 ... 9] = 10,
+          [10] = 80, 15, [70] = 50, [42] = 400 };
+ 
+    int i;
+    for (i = 0; i < 20; i++)   
+        printf("%d ", numbers[i]);
+     
+    printf("\n%d ", numbers[70]);
+    printf("%d", numbers[42]);
+}
