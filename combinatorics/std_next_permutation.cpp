cb29587b2fd75a38a66dc625b0456b24be5f5a16
netwanderer <128877043+nguyennamtien91@users.noreply.github.com>
2023-08-27 15:38:55 +0700

    Update std_next_permutation.cpp

--- combinatorics/std_next_permutation.cpp
+++ combinatorics/std_next_permutation.cpp
@@ -1,16 +1,17 @@
-class Solution 
-{
-public:
-    vector<vector<int>> permute(vector<int>& nums) 
-    {
-        vector<vector<int>>results;
-        sort(nums.begin(), nums.end());
-        results.push_back(nums);
+#include <bits/stdc++.h>
+using namespace std;
 
-        while(next_permutation(nums.begin(), nums.end()))
-        {
-            results.push_back(nums);
-        }
-        return results;
-    }
-};
+int main()
+{
+    vector<int> nums = {3, 1, 2};
+    sort(nums.begin(), nums.end());
+    
+    do
+    {
+        for(auto num : nums)
+            cout << num<< " ";
+        cout << endl;
+    } while (next_permutation(nums.begin(), nums.end()));
+
+    return 0;
+}
