a3937f9a5e26f228d366ad0ff7f55b78e7690b9b
netwanderer <128877043+nguyennamtien91@users.noreply.github.com>
2023-08-27 16:17:19 +0700

    Update array.cpp

--- data_structures/array.cpp
+++ data_structures/array.cpp
@@ -1,9 +1,7 @@
 #include <stdio.h>
 void main(void)
 {
-    int numbers[100] = {1, 2, 3, [3 ... 9] = 10,
-          [10] = 80, 15, [70] = 50, [42] = 400 };
- 
+    int numbers[100] = {1, 2, 3, [3 ... 9] = 10, [10] = 80, 15, [70] = 50, [42] = 400 };
     int i;
     for (i = 0; i < 20; i++)   
         printf("%d ", numbers[i]);
