78cb00a3563e47ecc95186e3a0f467bbb20e361b
NGUYEN NAM TIEN <128877043+Eldraena@users.noreply.github.com>
2023-11-10 12:09:20 +0700

    Update segment_tree.cpp

--- tree/segment_tree/segment_tree.cpp
+++ tree/segment_tree/segment_tree.cpp
@@ -9,16 +9,16 @@ struct segmenttree
 	vector<int> st;
 
 	void init(int _n) 
-    {
+    	{
 		this->n = _n;
 		st.resize(4 * n, 0);
 	}
 
 	void build(int start, int ending, int node, vector<int> &v) 
-    {
+    	{
 		// leaf node base case
 		if (start == ending) 
-        {
+        	{
 			st[node] = v[start];
 			return;
 		}
@@ -34,16 +34,16 @@ struct segmenttree
 	}
 
 	int query(int start, int ending, int l, int r, int node) 
-    {
+    	{
 		// non overlapping case
 		if (start > r || ending < l) 
-        {
+        	{
 			return 0;
 		}
 
 		// complete overlap
 		if (start >= l && ending <= r) 
-        {
+        	{
 			return st[node];
 		}
 
@@ -56,23 +56,23 @@ struct segmenttree
 	}
 
 	void update(int start, int ending, int node, int index, int value) 
-    {
+    	{
 		// base case
 		if (start == ending) 
-        {
+        	{
 			st[node] = value;
 			return;
 		}
 
 		int mid = (start + ending) / 2;
 		if (index <= mid) 
-        {
+        	{
 			// left subtree
 			update(start, mid, 2 * node + 1, index, value);
 		}
 		else 
-        {
-			// right
+	        {
+				// right
 			update(mid + 1, ending, 2 * node + 2, index, value);
 		}
 
@@ -81,17 +81,17 @@ struct segmenttree
 	}
 
 	void build(vector<int> &v) 
-    {
+    	{
 		build(0, n - 1, 0, v);
 	}
 
 	int query(int l, int r) 
-    {
+    	{
 		return query(0, n - 1, l, r, 0);
 	}
 
 	void update(int x, int y) 
-    {
+    	{
 		update(0, n - 1, 0, x, y);
 	}
 };
